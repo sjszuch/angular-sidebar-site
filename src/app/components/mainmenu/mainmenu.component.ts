7daac4707fa0b5d7a1f6639c8dcb29d9ccd4d5fb
Sebastian Szuch <sjszuch@gmail.com>
2023-05-22 15:16:06 -0400

    Init

---
+++ src/app/components/mainmenu/mainmenu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainmenu',
+  templateUrl: './mainmenu.component.html',
+  styleUrls: ['./mainmenu.component.scss']
+})
+export class MainmenuComponent {
+
+}
